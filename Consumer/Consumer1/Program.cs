46d19373362f519064d04dceb6a255357e999c3c
ayakubin <ayakubin@acumatica.com>
2019-07-20 07:35:53 +0300

    Демо

---
+++ Consumer/Consumer1/Program.cs
@@ -0,0 +1,81 @@
+﻿using MassTransit;
+using MassTransit.RabbitMqTransport;
+using Messages;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string host = "host";
+            string vhost = "vhost";
+            string username = "username";
+            string password = "password";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                IRabbitMqHost rmqHost = cfg.Host(host, vhost, h =>
+                {
+                    h.Username(username);
+                    h.Password(password);
+                });
+
+                cfg.ReceiveEndpoint(rmqHost, "consumer1-event-queue", e =>
+                {
+                    e.Consumer<EventConsumer>();
+                });
+
+                cfg.ReceiveEndpoint(rmqHost, "competing-consumer-queue", e =>
+                {
+                    e.Consumer<CompetingConsumer>();
+                });
+            });
+
+            bus.Start();
+
+            bool finish = false;
+
+            while(!finish)
+            {
+                await Task.Delay(1000);
+            }
+
+            bus.Stop();
+        }
+    }
+
+    public class EventConsumer : IConsumer<EventMessage>
+    {
+        public async Task Consume(ConsumeContext<EventMessage> context)
+        {
+            await Console.Out.WriteLineAsync(context.Message.Text);
+        }
+    }
+
+    public class CompetingConsumer : IConsumer<CompetingConsumersMessage>
+    {
+        public async Task Consume(ConsumeContext<CompetingConsumersMessage> context)
+        {
+            await Console.Out.WriteLineAsync(context.Message.Text);
+        }
+    }
+}
+
+namespace Messages
+{
+    public interface EventMessage
+    {
+        string Text { get; set; }
+    }
+
+    public interface CompetingConsumersMessage
+    {
+        string Text { get; set; }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,60 @@
+﻿using MassTransit;
+using MassTransit.RabbitMqTransport;
+using Messages;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string host = "host";
+            string vhost = "vhost";
+            string username = "username";
+            string password = "password";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                IRabbitMqHost rmqHost = cfg.Host(host, vhost, h =>
+                {
+                    h.Username(username);
+                    h.Password(password);
+                });
+            });
+
+            bus.Start();
+
+            Console.WriteLine("Нажмите Enter, чтобы опубликовать событийное сообщение.");
+            Console.ReadKey();
+
+            await bus.Publish<EventMessage>(new { Text = "Событийное сообщение: приходит ко всем потребителям." });
+
+            Console.WriteLine("Нажмите Enter, чтобы опубликовать соревновательное сообщение.");
+            Console.ReadKey();
+
+            await bus.Publish<CompetingConsumersMessage>(new { Text = "Соревновательное сообщение: принимается только одним из подписчиков очереди." });
+
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+}
+
+namespace Messages
+{
+    public interface EventMessage
+    {
+        string Text { get; set; }
+    }
+
+    public interface CompetingConsumersMessage
+    {
+        string Text { get; set; }
+    }
+}
